aa55b1a7287e092d27335583461f64a301e7896b
bonino97 <juanbonino97@gmail.com>
2021-02-14 01:41:54 -0300

    Adding feed component

--- src/app/global-feed/global-feed.module.ts
+++ src/app/global-feed/global-feed.module.ts
@@ -1,6 +1,9 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { FeedModule } from 'src/app/shared/modules/feed/feed.module';
+
 import { GlobalFeedComponent } from 'src/app/global-feed/components/global-feed/global-feed.component';
 
 const routes: Routes = [
@@ -12,6 +15,6 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [GlobalFeedComponent],
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [CommonModule, RouterModule.forChild(routes), FeedModule],
 })
 export class GlobalFeedModule {}
